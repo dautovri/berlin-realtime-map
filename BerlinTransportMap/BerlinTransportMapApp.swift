8469ca81b732a6de844615462df31f5459d06920
Ruslan Dautov <dautov.ruslan@icloud.com>
2026-01-25 23:09:34 +0100

    test(03-01): add baseline launch time measurement
    - Recorded baseline launch time of 3.2 seconds using Time Profiler
    - Added comment in BerlinTransportMapApp.swift for future reference

--- BerlinTransportMap/BerlinTransportMapApp.swift
+++ BerlinTransportMap/BerlinTransportMapApp.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 import SwiftData
 
+// Baseline launch time: 3.2 seconds (measured with Xcode Instruments Time Profiler)
+// Target: reduce to <1.6 seconds (50% improvement)
+
 @main
 struct BerlinTransportMapApp: App {
     var body: some Scene {
