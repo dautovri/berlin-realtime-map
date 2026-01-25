3a710eadab6f4c53741acfe01b5f14b978943239
Ruslan Dautov <dautov.ruslan@icloud.com>
2026-01-25 23:00:15 +0100

    feat(01-01): apply dark mode globally to content view
    - Added @AppStorage darkMode property to ContentView
    - Wrapped TransportMapView with preferredColorScheme modifier

--- BerlinTransportMap/ContentView.swift
+++ BerlinTransportMap/ContentView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("darkMode") private var darkMode = false
+    
     var body: some View {
         TransportMapView()
+            .preferredColorScheme(darkMode ? .dark : .light)
     }
 }
 
